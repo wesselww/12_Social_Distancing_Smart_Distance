089318bc87aba17fcc41d538a8f2d8ab866e7d6f
Nikolaus Schauersberger <nikolaus@arkulpa.at>
2020-03-22 10:49:20 +0100

    reformat code

--- app/src/main/java/de/wirvsvirus/smart_distance_control/MainActivity.java
+++ app/src/main/java/de/wirvsvirus/smart_distance_control/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.content.BroadcastReceiver;
@@ -32,13 +33,14 @@ public class MainActivity extends AppCompatActivity {
             String action = intent.getAction();
             if (BluetoothDevice.ACTION_FOUND.equals(action)) {
                 BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
-                int rssi = intent.getShortExtra(BluetoothDevice.EXTRA_RSSI,Short.MIN_VALUE);
-                RSSI2DISTANCE rssi2distance = new RSSI2DISTANCE(1.5,2.5);
+                int rssi = intent.getShortExtra(BluetoothDevice.EXTRA_RSSI, Short.MIN_VALUE);
+                RSSI2DISTANCE rssi2distance = new RSSI2DISTANCE(1.5, 2.5);
                 alarmManager.checkDistance((int) (rssi2distance.getDistance(rssi)));
-                Toast.makeText(getApplicationContext(),"Device Name:" + device.getName() +"  RSSI: " + rssi + "dBm "+"Abstand: "+ rssi2distance.getDistance(rssi) + "cm", Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), "Device Name:" + device.getName() + "  RSSI: " + rssi + "dBm " + "Abstand: " + rssi2distance.getDistance(rssi) + "cm", Toast.LENGTH_SHORT).show();
             }
         }
     };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -47,15 +49,15 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         ActionBar actionBar = getSupportActionBar();
-        this.BTAdapter = BluetoothAdapter .getDefaultAdapter();
+        this.BTAdapter = BluetoothAdapter.getDefaultAdapter();
 
 
-       alarmManager = new AlarmManager(getApplicationContext(), (ImageView) findViewById(R.id.alarm_icon));
+        alarmManager = new AlarmManager(getApplicationContext(), (ImageView) findViewById(R.id.alarm_icon));
 
 
-        BTAdapter = BluetoothAdapter .getDefaultAdapter();
+        BTAdapter = BluetoothAdapter.getDefaultAdapter();
 
-        if (BTAdapter == null){
+        if (BTAdapter == null) {
             new AlertDialog.Builder(this)
                     .setTitle("Error")
                     .setMessage("Your phone does not support Bluetooth")
@@ -63,32 +65,34 @@ public class MainActivity extends AppCompatActivity {
                         public void onClick(DialogInterface dialog, int which) {
                             System.exit(0);
                         }
-                     })
+                    })
                     .setIcon(android.R.drawable.ic_dialog_alert)
                     .show();
         }
         this.enableBluetooth();
         registerReceiver(bReciever, new IntentFilter(BluetoothDevice.ACTION_FOUND));
-            this.timerBTDiscovery = new CountDownTimer(10000,1000) {
-                @Override
-                public void onTick(long millisUntilFinished) {
+        this.timerBTDiscovery = new CountDownTimer(10000, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
 
-                }
+            }
 
-                @Override
-                public void onFinish() {
-                    BTAdapter.startDiscovery() ;
-                    timerBTDiscovery.start();
-                }
-            }.start();
+            @Override
+            public void onFinish() {
+                BTAdapter.startDiscovery();
+                timerBTDiscovery.start();
+            }
+        }.start();
     }
-    protected void enableBluetooth(){
-        if (!this.BTAdapter.isEnabled()){
-            Intent enableBT = new Intent (BluetoothAdapter.ACTION_REQUEST_ENABLE);
+
+    protected void enableBluetooth() {
+        if (!this.BTAdapter.isEnabled()) {
+            Intent enableBT = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
             startActivityForResult(enableBT, REQUEST_BLUETOOTH);
         }
     }
-    protected double getDistance(int rssi){
+
+    protected double getDistance(int rssi) {
         double dDistance = 0.0;
 
         return dDistance;
@@ -98,9 +102,10 @@ public class MainActivity extends AppCompatActivity {
     public void alarmbutton(View v) {
         alarmManager.alarmbutton();
     }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()){
+        switch (item.getItemId()) {
             case R.id.tbSettings:
                 startActivity(new Intent(MainActivity.this, Settings.class));
                 break;
@@ -109,6 +114,7 @@ public class MainActivity extends AppCompatActivity {
         }
         return true;
     }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
